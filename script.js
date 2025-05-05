886cb3a2b3ff54568195da244a43046f28059647
daniel05122008 <daniel.furlaneto@escola.pr.gov.br>
2025-05-05 00:05:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const toggleButton = document.getElementById('toggle-background');
+const body = document.body;
 
+toggleButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+});
